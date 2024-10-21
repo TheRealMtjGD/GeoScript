e8cc17faea05aa120abf2062fcccb17a4963071e
MtjGD <ranieldees@gmail.com>
2024-10-21 12:23:55 +0100

    Fixed broken STDLIB (#16)

--- INSTALL_SCRIPTS/install-windows.bat
+++ INSTALL_SCRIPTS/install-windows.bat
@@ -13,6 +13,8 @@ robocopy . %GSPATH%
 echo "Installing STDLIB"
 cd %GSPATH%
 git clone https://github.com/TheRealMtjGD/GSLIB.git
+rmdir Modules
+rename GSLIB Modules
 
 echo "Creating config file"
 echo "gd_main_path=%GD_MAIN_PATH%\ngd_appdata_path=%GD_APPDATA_PATH%" > %GSPATH%/.env

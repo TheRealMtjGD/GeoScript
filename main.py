52b12612aa7d83b85ab95a0ec5048b6c3df688d5
MtjGD <ranieldees@gmail.com>
2024-10-22 20:52:48 +0000

    Main frontend file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Components import error_handler
 from Components import gsconfig
 from Components import env_loader
 from Components import memory_manager
-
+from Components import logging
 import gsparser
 import backup
 import abstract_st
@@ -10,11 +10,11 @@ import type_checking
 import module_compiler
 import gsc_compile
 
-def load_enviroments() -> list[dict]:
+def load_enviroments() -> tuple[dict]:
     main_config = env_loader.loadDotEnv('.env')
     compile_config = env_loader.loadDotEnv('config.env')
     
-    return [main_config, compile_config]
+    return (main_config, compile_config)
 
 def check_compile_requirements(file: str) -> dict:
     config = {}
@@ -51,3 +51,54 @@ def loadGSCRequirements(input: str) -> dict:
 def dumpGSCRequirements(ast: dict, ct: dict, hm: memory_manager.InitHeapMemory, ids: memory_manager.IdentifierStack, mh: int, output: str) -> None:
     memdict = {'hm': hm.heap_memory, 'ids': ids.id_stack, 'max-heap': mh}
     gsc_compile.encodeGSCFile(ast, ct, memdict, output)
+
+
+
+def main() -> None:
+    main_env, config_env = load_enviroments()
+    compile_requirements = check_compile_requirements(config_env['file'])
+    
+    # creating backups
+    logging.logToFile(logging.P, 'Creating backups', logging.CompileLog)
+    backup.backupSavefile(f'{main_env['gd_appdata_loc']}/CCLocalLevels.dat')
+    
+    # compile mode
+    if config_env['buildmode'] == 'std':
+        if compile_requirements['compile-mode'] == 'Standard':
+            logging.logToFile(logging.P, 'Starting compilation (mode: standard)', logging.CompileLog)
+            
+            logging.logToFile(logging.S, 'Creating pointer heap and identifier stack', logging.CompileLog)
+            heap_memory = memory_manager.InitHeapMemory(int(main_env['heap_memory_max']))
+            identifier_stack = memory_manager.IdentifierStack()
+            
+            # frontend
+            logging.logToFile(logging.P, 'Compiling modules into gspackage', logging.CompileLog)
+            with open(config_env['file']) as rstream:
+                gspackage = module_compiler.compileModules(rstream.read(), ...)
+            
+            logging.logToFile(logging.P, 'Tokenising files', logging.CompileLog)
+            tokenised = gsparser.GSParser(gspackage, config_env['file'], {'hm': heap_memory, 'ids': identifier_stack})
+            
+            logging.logToFile(logging.P, 'Parsing abstract syntax tree', logging.CompileLog)
+            abstract_syntax_tree = abstract_st.createAbstractSyntaxTree(tokenised)
+        
+        elif compile_requirements['compile-mode'] == 'QuickCompile':
+            ...
+    
+    else:
+        logging.logToFile(logging.P, 'Starting compilation (mode: standard)', logging.CompileLog)
+            
+        logging.logToFile(logging.S, 'Creating pointer heap and identifier stack', logging.CompileLog)
+        heap_memory = memory_manager.InitHeapMemory(int(main_env['heap_memory_max']))
+        identifier_stack = memory_manager.IdentifierStack()
+            
+        # frontend
+        logging.logToFile(logging.P, 'Compiling modules into gspackage', logging.CompileLog)
+        with open(config_env['file']) as rstream:
+            gspackage = module_compiler.compileModules(rstream.read(), ...)
+            
+        logging.logToFile(logging.P, 'Tokenising files', logging.CompileLog)
+        tokenised = gsparser.GSParser(gspackage, config_env['file'], {'hm': heap_memory, 'ids': identifier_stack})
+            
+        logging.logToFile(logging.P, 'Parsing abstract syntax tree', logging.CompileLog)
+        abstract_syntax_tree = abstract_st.createAbstractSyntaxTree(tokenised)

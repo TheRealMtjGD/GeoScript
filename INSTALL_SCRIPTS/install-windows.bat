d1361f04b5faa5fc3f12adb83bb1dc224fe5f00f
MtjGD <ranieldees@gmail.com>
2024-10-23 22:52:02 +0100

    Fixed windows rename (#17)

--- INSTALL_SCRIPTS/install-windows.bat
+++ INSTALL_SCRIPTS/install-windows.bat
@@ -14,7 +14,7 @@ echo "Installing STDLIB"
 cd %GSPATH%
 git clone https://github.com/TheRealMtjGD/GSLIB.git
 rmdir Modules
-rename GSLIB Modules
+move GSLIB Modules
 
 echo "Creating config file"
 echo "gd_main_path=%GD_MAIN_PATH%\ngd_appdata_path=%GD_APPDATA_PATH%" > %GSPATH%/.env
